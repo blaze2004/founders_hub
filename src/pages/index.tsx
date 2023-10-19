be37578177912ae3d042b54932809963637df4f9
Nitin <nitinrana01125532553@gmail.com>
2023-10-19 18:58:52 +0530

    fixed small responsive details in hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Hero from '@/components/index/hero';
 const Home=() => {
   return (
     <>
-      <div className='flex w-full flex-col items-center py-32 bg-black text-white'>
+      <div className='flex w-full p-8 flex-col items-center py-32 bg-black text-white'>
         <Hero />
       </div>
       <div className='flex w-full flex-col items-center py-32'>
