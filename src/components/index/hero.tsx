767da056355810d8c50f9f6ca326db7057925201
perplexedfor <gusainabhinav037@gmail.com>
2023-10-19 15:25:55 +0530

    first commit added gradient

--- src/components/index/hero.tsx
+++ src/components/index/hero.tsx
@@ -2,12 +2,12 @@ const Hero=() => {
   return (
     <div>
       <h1
-        className='mt-8 text-center font-display text-4xl font-bold text-transparent drop-shadow-sm md:text-7xl'
+        className='mt-8 text-center font-display text-4xl font-bold text-transparent drop-shadow-sm md:text-7xl gradient__text'
       >
         Join the World&apos;s Most
       </h1>
       <h1
-        className='bg-gradient-to-br from-light-purple mb-4 to-deep-purple bg-clip-text text-center font-display text-4xl font-bold text-transparent drop-shadow-sm md:text-7xl'
+        className='bg-gradient-to-br from-light-purple mb-4 to-deep-purple bg-clip-text text-center font-display text-4xl font-bold text-transparent drop-shadow-sm md:text-7xl gradient__text'
       >
         Passionate Startup Community
       </h1>
